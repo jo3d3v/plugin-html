27d80026bf3bc6d9b8418d30dd10107cea20ebfe
jo3.d3v <jo3.d3v@gmail.com>
2017-04-02 00:37:50 +0200

    cleanup

--- html.js
+++ html.js
@@ -1,16 +1,14 @@
-var htmlclean = require('htmlclean');
+var cleaner = require('htmlclean');
 
 /**
  * HTML plugin
  */
 exports.translate = function (load) {
-    return SystemJS['import']('htmlclean', module.id).then(function (cleaner) {
-        if (this.builder && this.transpiler) {
-            load.metadata.format = 'esm';
-            return 'exp' + 'ort var __useDefault = true; exp' + 'ort default ' + JSON.stringify(cleaner.htmlclean(load.source)) + ';';
-        }
+    if (this.builder && this.transpiler) {
+        load.metadata.format = 'esm';
+        return 'exp' + 'ort var __useDefault = true; exp' + 'ort default ' + JSON.stringify(cleaner.htmlclean(load.source)) + ';';
+    }
 
-        load.metadata.format = 'amd';
-        return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(cleaner.htmlclean(load.source)) + ';\n});';
-    });
+    load.metadata.format = 'amd';
+    return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(cleaner.htmlclean(load.source)) + ';\n});';
 }
